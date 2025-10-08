39fa9d6c8fec3233f61827169f9237f0aa6a9b6e
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-10-07 22:40:45 -0300

    Finaliazado a logica principal agora só falta add o scanner

---
+++ src/ex01/Dados.java
@@ -0,0 +1,20 @@
+package ex01;
+
+public class Dados {
+	public double width;
+	public double height;
+	public double area;
+	
+
+	public double Area() {
+		return area = width * height;
+	}
+	
+	public double perimetro(){
+		return 2 * (width + height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+	}
+}
